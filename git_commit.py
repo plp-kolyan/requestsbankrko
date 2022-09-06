ab52ecaa0ec197dc5f4c8e47c970659f77d2ac76
DarkRastafar <mvmsurgutmebel@gmail.com>
2022-09-06 10:38:46 +0500

    'development branch test'

---
+++ git_commit.py
@@ -0,0 +1,23 @@
+import os
+import hashlib
+from update_library_version import update_version
+
+
+def update_toml():
+    start_hash = hashlib.md5(open('pyproject.toml', 'rb').read()).hexdigest()
+    update_version()
+
+    if start_hash != hashlib.md5(open('pyproject.toml', 'rb').read()).hexdigest():
+        return True
+
+
+def start():
+    if update_toml() is True:
+        os.system('git add .')
+        os.system(f'git commit -m "{input("Напиши текст коммита: ")}"')
+        os.system('git branch -M development')
+        os.system('git push -u origin development')
+
+
+if __name__ == '__main__':
+    start()
---
+++ update_library_version.py
@@ -0,0 +1,34 @@
+
+
+def up_version(version_list, index=None):
+    if index is None:
+        last_index = len(version_list) - 1
+    else:
+        last_index = index
+    version_list[last_index] = str(int(version_list[last_index]) + 1)
+
+    if last_index == 0:
+        return '.'.join(version_list)
+    else:
+        last_chr = int(version_list[last_index])
+        if last_chr > 9:
+            version_list[last_index] = str(last_chr // 10)
+            last_index -= 1
+            return up_version(version_list, last_index)
+        return '.'.join(version_list)
+
+
+def update_version(path_to_file="pyproject.toml"):
+    import toml
+    data = {}
+
+    with open(path_to_file, "r", encoding="UTF-8") as file:
+        data = toml.load(file)
+        version_list = data['project']['version'].split('.')
+        data['project']['version'] = up_version(version_list)
+    with open(path_to_file, "w", encoding="UTF-8") as file:
+        toml.dump(data, file).encode('utf-8')
+
+
+if __name__ == '__main__':
+    update_version()
